b45ebcd2d405dc6d2ba55f0cd6f1423a75bc0295
Danniel Rolfe <dannielrolfe@umpquabank.com>
2021-09-09 21:27:49 -0700

    Update login

--- projects/about/src/lib/about.component.ts
+++ projects/about/src/lib/about.component.ts
@@ -3,9 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-about',
   template: `
-    <p>
-      about works!
-    </p>
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">AWESOME APP</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Dashboard</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Admin</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <main class="flex-shrink-0">
+      <div class="container">
+        <div class="row mt-5">
+          <div class="col-8 offset-2">
+            <h1 class="mt-5">Welcome</h1>
+            <p>to the world of Micro Front-Ends</p>
+            <img class="img-fluid rounded" alt="Funny picture" src="https://i.pinimg.com/originals/4f/82/8d/4f828d05f82b8b7aedfe8be6a7d9d2a3.png">
+          </div>
+        </div>
+      </div>
+    </main>
   `,
   styles: [
   ]
